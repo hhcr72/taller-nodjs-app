2d8bfca78b29b75e4310a64d144110edc12de545
hhcr72 <lschhcordero@gmail.com>
2021-06-21 19:53:52 -0500

    modificacion datos de acceso a bd

--- src/keys.js
+++ src/keys.js
@@ -1,12 +1,12 @@
 module.exports = {
 
     database:  {
-        //host: 'localhost',
-        //user 'root',
-        host: '192.168.70.254',
-        user: 'hugo',
-        password: 'aposal08576',
-        database: 'taller'
+        //host: '',
+        //user '',
+        host: '',
+        user: '',
+        password: '',
+        database: ''
     }
 
 };
